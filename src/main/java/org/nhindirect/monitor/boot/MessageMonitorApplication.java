c9b992aecc1fb86cde03b1b96306b1dacb9e5be4
gm2552 <gm2552@localhost>
2018-10-11 12:03:34 -0500

    Moved EnableJPARepositories to biz module (direct-msg-monitor).

--- src/main/java/org/nhindirect/monitor/boot/MessageMonitorApplication.java
+++ src/main/java/org/nhindirect/monitor/boot/MessageMonitorApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan(basePackages = {"org.nhindirect.monitor"})
 @SpringBootApplication
 @Configuration
-@EnableJpaRepositories("org.nhindirect.monitor.repository")
 @EnableEurekaClient
 public class MessageMonitorApplication
 {
