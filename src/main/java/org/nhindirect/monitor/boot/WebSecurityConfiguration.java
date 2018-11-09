aa844d81342e5e120cf87c0c0c1ac0bb558408ee
gm2552 <gm2552@localhost>
2018-11-09 10:37:49 -0600

    Moving to webflux.

--- src/main/java/org/nhindirect/monitor/boot/WebSecurityConfiguration.java
+++ src/main/java/org/nhindirect/monitor/boot/WebSecurityConfiguration.java
@@ -1,15 +1,18 @@
 package org.nhindirect.monitor.boot;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
+@EnableWebFluxSecurity
+public class WebSecurityConfiguration
 {
-    @Override
-    public void configure(WebSecurity web) throws Exception 
-    {
-    	web.ignoring().antMatchers("/actuator/**");
-    } 
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http)
+	{
+		http.csrf().disable();
+
+	    return http.build(); 
+	}
 }
