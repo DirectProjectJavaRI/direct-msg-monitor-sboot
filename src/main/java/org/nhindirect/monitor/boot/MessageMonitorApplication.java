7b60250415846e88b5abc5ad158bdc1c567a7144
gm2552 <gm2552@xmppserver.cernerdirectsupport.com>
2021-07-28 18:30:06 -0500

    Initial Direct RI 8.0.0 checkin.

--- src/main/java/org/nhindirect/monitor/boot/MessageMonitorApplication.java
+++ src/main/java/org/nhindirect/monitor/boot/MessageMonitorApplication.java
@@ -2,14 +2,12 @@ package org.nhindirect.monitor.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(basePackages = {"org.nhindirect.monitor"})
 @SpringBootApplication
 @Configuration
-@EnableEurekaClient
 public class MessageMonitorApplication
 {
     public static void main(String[] args) 
